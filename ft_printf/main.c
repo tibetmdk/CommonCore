c9844e953eb1c3f87cac48aed8343819c535f11c
Tibet Mıdık <tmidik@k1m43s03.42istanbul.com.tr>
2024-11-07 18:08:07 +0300

    asd

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -4,6 +4,21 @@
 int main()
 {
 	int a = 15234;
-	ft_printf("%cx%cx%c\n", '0', 0, 1);
-	printf("%cx%cx%c\n", '0', 0, 1);
+	printf("%d", 19);
+	write(1, &"berk", 4);
+	write(1, &"merk", 4);
+	write(1, &"cerk", 4);
+	write(1, &"derk", 4);
+	write(1, &"aerk", 4);
+	write(1, &"berk", 4);
+	write(1, &"merk", 4);
+	write(1, &"cerk", 4);
+	write(1, &"derk", 4);
+	write(1, &"aerk", 4);
+	write(1, &"berk", 4);
+	write(1, &"merk", 4);
+	write(1, &"cerk", 4);
+	write(1, &"derk", 4);
+	write(1, &"aerk", 4);
+	ft_printf("%d", 10);
 }
